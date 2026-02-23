7ec5d0037d3ea51fc9529ec2a28cf70decf22829
B0bbyBrown <209124054+EFLExplorers@users.noreply.github.com>
2026-02-23 16:22:03 +0200

    Update _document.tsx

--- apps/landing-page/src/pages/_document.tsx
+++ apps/landing-page/src/pages/_document.tsx
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Preload critical assets */}
-        <link
-          rel="preload"
-          href="/assets/images/logo/Logo.png"
-          as="image"
-          type="image/png"
-        />
-        
-        {/* Preload critical CSS */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
